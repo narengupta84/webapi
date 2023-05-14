a5fd97a37accc515cd37681c2070295f5fb3aaf9
Narendra <narengupta84@gmail.com>
2023-05-14 22:28:41 +0700

    feat: :wrench: Enable Token Bearer authentication in Swagger

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -17,20 +17,37 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen(options =>
 {
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
+    //options.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
     options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
-        Description = @"JWT Authorization header using the Bearer scheme. \n\n 
+        Description = @"JWT Authorization header using the Bearer scheme.<br/> 
                       Enter 'Bearer' [space] and then your token in the text input below.
-                      \n\nExample: 'Bearer xxxxx.yyyyy.zzzzz'",
+                      <br/> Example: 'Bearer xxxxx.yyyyy.zzzzz'",
         Name = "Authorization",
         In = ParameterLocation.Header,
         Type = SecuritySchemeType.ApiKey,
         Scheme = "Bearer"
     });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
 });
 
-//Begin JWT
+#region JWT
 var key = "webapijwttokenkey1234567890";
 builder.Services.AddAuthentication(x =>
 {
@@ -49,7 +66,7 @@ builder.Services.AddAuthentication(x =>
     };
 });
 builder.Services.AddSingleton<JwtAuthenticationManager>(new JwtAuthenticationManager(key));
-//End JWT
+#endregion JWT
 
 builder.Services.AddTransient<Seed>();
 builder.Services.AddControllers().AddJsonOptions(x =>
